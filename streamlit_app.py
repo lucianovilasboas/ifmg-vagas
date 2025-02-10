7c8d3f1a64ff8a7b8b0877f217bea0fca61e0ad7
Luciano <lucianovilasboas@gmail.com>
2025-02-10 20:36:23 -0300

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,20 @@
 import streamlit as st
 import pandas as pd
 import io
-from config import ordem_form, fluxo, filtros, regras_remanejamento, filter_situacao_geral, color_map
+from config import ordem_form, fluxo, filtros, regras_remanejamento, filter_situacao_geral, color_map, campus_id, campus_curso_id, cota_id
 
 st.set_page_config(page_title="Ocupação de Vagas - IFMG", page_icon="🎯", layout="wide")
+
+
+# Verifica se os arquivos já estão salvos no session_state
+if "output_xlsx" not in st.session_state:
+    st.session_state["output_xlsx"] = None
+
+if "output_csv" not in st.session_state:
+    st.session_state["output_csv"] = None
+
+
+
 
 vagas = {}
 vagas_nao_ocupadas = {}
@@ -80,6 +91,35 @@ if uploaded_file is not None:
                 return [f'background-color: {color}; color: black;'] * len(row)
 
 
+            def gerar_carga_de_dados(df: pd.DataFrame):
+                """
+                    Gera a carga de dados para sitema de matrícula.
+                    ```csv
+                        CPF (no formato xxx.xxx.xxx-xx),ID do Campus,ID do curso,ID do edital,ID da cota inscrito,ID da cota chamada,Nº de inscrição,Classificação;
+
+                    ```
+                """
+                # selecionar as colunas necessárias para a carga de dados
+                cols = ["CPF do candidato", "Campus", "Curso", "Grupo de vagas inscrito", "Grupo_vagas_chamado_", "Inscrição", "Classificação Geral"]
+
+                df_filter = df[df["Grupo_vagas_chamado_"] != ""][cols].copy()
+                campus = df_filter["Campus"].iloc[0].replace("Campus ", "").strip()
+
+                # print("Campus ->"    , f"'{campus}'")
+
+                df_filter["ID_Campus"] = campus_id.get(campus, 0)
+                df_filter["ID_Curso"] = df_filter["Curso"].apply(lambda c: campus_curso_id.get(campus, {}).get(c.split(" - ")[0], ""))    
+                df_filter["ID_Edital"] = "<preenchido pelo campus>"
+                df_filter["Grupo de vagas inscrito"] = df_filter["Grupo de vagas inscrito"].apply(lambda c: cota_id.get(c, 0))
+                df_filter["Grupo_vagas_chamado_"] = df_filter["Grupo_vagas_chamado_"].apply(lambda c: cota_id.get(c, 0))
+                df_filter["Classificação Geral"] = df_filter["Classificação Geral"].apply(lambda i: f"{i};")
+
+                order_cols = ["CPF do candidato", "ID_Campus", "ID_Curso", "ID_Edital", "Grupo de vagas inscrito", "Grupo_vagas_chamado_", "Inscrição", "Classificação Geral"]
+
+                return df_filter[order_cols]
+
+
+
             # --- Funções para ocupação de vagas ---
 
 
@@ -121,7 +161,7 @@ if uploaded_file is not None:
                                             .head(num_vagas)
                 
                 vagas_nao_ocupadas[grupo_vagas_inscrito] = num_vagas - linhas_filtradas.shape[0]
-                print(grupo_vagas_inscrito,f"num_vagas: [{num_vagas}]", "->",vagas_nao_ocupadas[grupo_vagas_inscrito])
+                # print(grupo_vagas_inscrito,f"num_vagas: [{num_vagas}]", "->",vagas_nao_ocupadas[grupo_vagas_inscrito])
                 
                 if linhas_filtradas.shape[0] > 0:
                     # Atribui valores às colunas `Grupo_vagas_inicial_` e `Grupo_vagas_chamado_`
@@ -157,23 +197,27 @@ if uploaded_file is not None:
                                 df_filter.loc[linhas_filtradas.index, "Log"] = "Vaga remanejada"
                 
                                 vagas_nao_ocupadas[cota] = n_vagas_restantes - linhas_filtradas.shape[0]
-                                print(" |=>> ",vagas_nao_ocupadas[cota])
+                                # print(" |=>> ",vagas_nao_ocupadas[cota])
                 
                             if vagas_nao_ocupadas[cota] <= 0: 
                                 print(f"Encerrou a ocupação da vaga para a cota {cota}")
                                 break
 
             
+            # --- Fim das funções de ocupação de vagas ---
+
+            # iniciar a ocupação de vagas
+            # Selecionar as colunas necessárias para a ocupação
+
             # df_filter = df[(df["Curso"] == curso_selecionado) & (df["Situação Geral"].isin(filter_situacao_geral)) ][cols_all]
             df_filter = df[(df["Curso"] == curso_selecionado) & (df["Situação Geral"].isin(filter_situacao_geral)) ]
 
             ocupar_vagas(df_filter)
             
+
             st.subheader("Resultado da Ocupação")
-
-            df_filter["Confere_1"] = df_filter["Grupo de vagas inicial"] ==  df_filter["Grupo_vagas_inicial_"]
-            df_filter["Confere_2"] = df_filter["Grupo de vagas chamado"] ==  df_filter["Grupo_vagas_chamado_"] 
-
+            # df_filter["Confere_1"] = df_filter["Grupo de vagas inicial"] ==  df_filter["Grupo_vagas_inicial_"]
+            # df_filter["Confere_2"] = df_filter["Grupo de vagas chamado"] ==  df_filter["Grupo_vagas_chamado_"] 
 
             # styled_df = df_filter.style.applymap(highlight_grades, subset=['Total'])
             # styled_df = df_filter.style.apply(highlight_mismatch, axis=1)
@@ -184,10 +228,54 @@ if uploaded_file is not None:
             campus_ = campus[0].replace(" ", "_")
             curso_selecionado_ = curso_selecionado.replace(" ", "_")
             
-            output = io.BytesIO()
-            with pd.ExcelWriter(output, engine="xlsxwriter") as writer:
+            output_xlsx = io.BytesIO()
+            with pd.ExcelWriter(output_xlsx, engine="xlsxwriter") as writer:
                 df_filter.to_excel(writer, index=False, sheet_name="Resultado")
-            output.seek(0)
+            output_xlsx.seek(0)
 
 
-            st.download_button("Baixar resultado em .xlsx", output, file_name=f"Resultado_Ocupacao_{curso_selecionado_}.xlsx", mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+
+
+            st.subheader("Carga de Dados para Matrícula")
+            df_carga = gerar_carga_de_dados(df_filter)
+            st.dataframe(df_carga)
+
+            # Criando o arquivo CSV em memória
+            csv_buffer = io.StringIO()
+            df_carga.to_csv(csv_buffer, header=False, index=False)
+            csv_data = csv_buffer.getvalue()  # Obtém os dados em string
+
+            
+            # Armazena os arquivos no session_state para evitar que sejam recriados a cada reload
+            st.session_state["output_xlsx"] = output_xlsx
+            st.session_state["output_csv"] = csv_data
+
+
+
+        # Se os arquivos já foram gerados, exibe os botões de download
+        if st.session_state["output_xlsx"] is not None and st.session_state["output_csv"] is not None:
+            
+            st.success(f"Processamento concluído para {curso_selecionado}! Baixe os arquivos abaixo.")
+
+            col1, col2 = st.columns(2)
+
+            with col1:
+                st.download_button(
+                    "Baixar resultado em .xlsx",
+                    st.session_state["output_xlsx"].getvalue(),
+                    file_name=f"Resultado_Ocupacao_{curso_selecionado}.xlsx",
+                    mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                )
+
+            with col2:
+                st.download_button(
+                    "Baixar Carga de Dados para Matrícula",
+                    st.session_state["output_csv"],
+                    file_name=f"Carga_{curso_selecionado}.csv",
+                    mime="text/csv"
+                )
+
+
+            # st.download_button("Baixar resultado em .xlsx", output, file_name=f"Resultado_Ocupacao_{curso_selecionado_}.xlsx", mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+            # st.download_button("Baixar Carga de Dados para Matrícula", f"Carga_{curso_selecionado_}.csv", "Carga de Dados", "csv")
+
